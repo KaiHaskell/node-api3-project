f8a40421580bf8fcfbbe645a457f5265d43c87ba
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-02-14 03:30:47 -0500

    not working properly

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const helmet = require("helmet");
+const userRouter = require("./users/userRouter");
+
 const server = express();
+server.use(express.json());
 
 //Routers
-const userRouter = require("./users/userRouter");
-server.use("/api/users", userRouter);
 
-server.use(express.json());
-server.use(helmet());
+server.use("/api/users", logger, userRouter);
 
-server.get("/", logger, (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -4,19 +4,34 @@ const Posts = require("../posts/postDb");
 
 const router = express.Router();
 
-//get a list of all users
+//get a list of all users ✅
 router.get("/", (req, res) => {
-  // do your magic!
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Unable to fetch list of users" });
+    });
 });
 
-//get a specific user
-router.get("/:id", (req, res) => {
-  // do your magic!
+//get a specific user ✅
+router.get("/:id", validateUserId, (req, res) => {
+  Users.getById(req.params.id)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Unable to fetch that user" });
+    });
 });
 
-//create new user
-router.post("/", (req, res) => {
-  User.insert(req.body)
+//create new user ❌
+router.post("/", validateUser, (req, res) => {
+  console.log(req.name);
+  Users.insert(req.name)
     .then(user => {
       res.status(201).json(user);
     })
@@ -28,7 +43,7 @@ router.post("/", (req, res) => {
     });
 });
 
-//create new post for a user
+//create new post for a user ❌
 router.post("/:id/posts", (req, res) => {
   const postInfo = { ...req.body, user_id: req.params.id };
   Posts.insert(postInfo)
@@ -43,13 +58,35 @@ router.post("/:id/posts", (req, res) => {
     });
 });
 
-//get a specific users complete post history
+//get a specific users complete post history ✅
 router.get("/:id/posts", (req, res) => {
-  Users.getUsee;
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ ERROR: "Unable to retrieve that users posts" });
+    });
 });
 
+//delete a specific user ✅
 router.delete("/:id", (req, res) => {
-  // do your magic!
+  Users.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "The user has been removed" });
+      } else {
+        res.status(404).json({ message: "The user could not be found" });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the hub"
+      });
+    });
 });
 
 router.put("/:id", (req, res) => {
@@ -58,16 +95,44 @@ router.put("/:id", (req, res) => {
 
 //custom middleware
 
+// WORKING ✅
 function validateUserId(req, res, next) {
-  // do your magic!
+  const id = req.params.id;
+  console.log("\nid\n", id);
+
+  Users.getById(id).then(user => {
+    if (user) {
+      console.log("user", user);
+      next();
+    } else {
+      res.status(400).json({ message: "User does not exist" });
+    }
+  });
 }
 
+//WORKING ❓
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  }
+  console.log(req.body);
+  next();
 }
 
+//WORKING ❓
 function validatePost(req, res, next) {
-  // do your magic!
+  const user_id = req.params.id;
+  const body = { ...req.body, user_id: user_id };
+  console.log("user_id", user_id, "body", body);
+  if (body.text) {
+    console.log("has text");
+    next();
+  } else {
+    console.log("has no text");
+    res.status(400).json({ message: "missing required text field" });
+  }
 }
 
 module.exports = router;
